95b951f0d5a428c3cf59e69400099b47175810fd
Camille Roussel <camille@rousselfamily.com>
2014-12-11 13:13:21 -0500

    implemented map

--- src/underbar.js
+++ src/underbar.js
@@ -121,6 +121,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+
+    _.each(collection, function(elem) {
+      results.push(iterator(elem));
+    });
+
+    return results;
   };
 
   /*
