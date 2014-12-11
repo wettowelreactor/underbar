fdba574cbb5f7e3b0d64810a65dc75be0138d683
Camille Roussel <camille@rousselfamily.com>
2014-12-11 12:50:21 -0500

    implemented filter

--- src/underbar.js
+++ src/underbar.js
@@ -82,6 +82,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+
+    _.each(collection, function(item) {
+      if (test(item)) {
+        results.push(item);
+      }
+    });
+
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
