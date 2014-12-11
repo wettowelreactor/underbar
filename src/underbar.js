e1a15ab3bae3ea4e5529b63b1e84fc6dffd3adab
Camille Roussel <camille@rousselfamily.com>
2014-12-11 12:56:45 -0500

    implemeneted reject

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,9 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(elem) {
+      return !test(elem);
+    });
   };
 
   // Produce a duplicate-free version of the array.
